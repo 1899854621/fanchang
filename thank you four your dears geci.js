361ecfb6d427a346cb62cf31c697109503c847bf
mengxi丶 <159696620+1899854621@users.noreply.github.com>
2025-11-15 19:44:36 +0800

    Update thank you four your dears geci.js

--- thank you four your dears geci.js
+++ thank you four your dears geci.js
@@ -14,303 +14,307 @@ const lrc = `[ar:GET IN THE RING]
 
 
 [00:29.58]Can you hear my calling even if its raining
-
+[00:29.58]你能听见我的呼唤吗 穿过那沥沥雨声
 
 [00:31.51]Flowers coming into bloom
-
+[00:31.51]繁花盛开 争相斗艳
 
 [00:33.89]Looking for the shining every night and morning
-
+[00:33.89]夜以继日 不断寻找那片光明
 
 [00:36.11]This ties will never broke again
-
+[00:36.11]夜以继日 不断寻找那片光明
 
 [00:38.25]You will know how much I love you
-
+[00:38.25]你终会明白 我爱你有多深
 
 [00:39.78]Things that I can tell you is wanna be live with you all
-
+[00:39.78]千言万语汇成一句话 我想和你一起共度余生
 
 [00:42.83]And I promise you forever walking with together
-
+[00:42.83]执子之手 与子偕老 这是我对你的海誓山盟
 
 [00:45.24]Giving a thanks to my dears
-
+[00:45.24]执子之手 与子偕老 这是我对你的海誓山盟
 
 [00:47.76]目が覚めた暗闇で
-
+[00:47.76]在黑暗之中睁眼苏醒过来
 
 [00:51.68]見えたモノが瞬いて
-
+[00:51.68]眼前的一切皆闪耀着光芒
 
 [00:56.12]「懐かしい」と
-
+[00:56.12]下意识说道“真令人怀念”
 
 [00:58.85]胸を締め付けてる
-
+[00:58.85]这种感觉紧缚着我的胸口
 
 [01:01.09]痛み鼓動同じモノね
-
+[01:01.09]痛楚 心跳 都是一样的呢
 
 [01:05.94]気付いたときに
-
+[01:05.94]在察觉到这一点的时候
 
 [01:10.76]気付かせてくれる
-
+[01:10.76]才后知后觉
 
 [01:14.44]仲間と共に歩くことが
-
+[01:14.44]未来也能与同伴一起
 
 [01:20.10]できるのだから
-
+[01:20.10]继续携手向前迈进
 
 [01:24.37]ほら記憶が導いてから
-
+[01:24.37]你看 记忆会为我们指引方向
 
 [01:25.17]Can you hear my calling even if its raining
-
+[01:25.17]你能听见我的呼唤吗 穿过那沥沥雨声
 
 [01:27.04]Flowers coming into bloom
-
+[01:27.04]繁花盛开 争相斗艳
 
 [01:29.14]Looking for the shining every night and morning
-
+[01:29.14]夜以继日 不断寻找那片光明
 
 [01:31.57]This ties will never broke again
-
+[01:31.57]我们如胶似漆 谁也无法再次将我们分开
 
 [01:33.60]You will know how much I love you
-
+[01:33.60]你终会明白 我爱你有多深
 
 [01:35.21]Things that I can tell you is wanna be live with you all
-
+[01:35.21]千言万语汇成一句话 我想和你一起共度余生
 
 [01:38.21]And I promise you forever walking with together
-
+[01:38.21]执子之手 与子偕老 这是我对你的海誓山盟
 
 [01:40.68]Giving a thanks to my dears
-
+[01:40.68]我想对你说 谢谢你 我的爱人
 
 [01:42.86]星満ちるこの夜に
-
+[01:42.86]在繁星满天的夜里
 
 [01:46.97]一筋流れた願い事
-
+[01:46.97]对着划过天际的流星许下心愿
 
 [01:51.54]「このままがいい」
-
+[01:51.54]“继续这样就好了”
 
 [01:54.38]心の底からの
-
+[01:54.38]发自心底的
 
 [01:56.36]叫び祈り似て非なるものね
-
+[01:56.36]呐喊 祈祷 总是似是而非
 
 [02:01.64]寂しいときに
-
+[02:01.64]在我感到寂寞的时候
 
 [02:05.96]傍にいてくれる
-
+[02:05.96]总会默默陪伴在我身旁
 
 [02:09.65]仲間と共に過ごすことが
-
+[02:09.65]未来也能与同伴一起
 
 [02:15.49]できるのだから
-
+[02:15.49]继续携手相伴共度
 
 [02:19.71]ほら未來が輝いてるから
-
+[02:19.71]你看 未来正闪耀着光芒
 
 [02:20.53]Can you hear my calling even if its raining
-
+[02:20.53]你能听见我的呼唤吗 穿过那沥沥雨声
 
 [02:22.28]Flowers coming into bloom
-
+[02:22.28]繁花盛开 争相斗艳
 
 [02:24.61]Looking for the shining every night and morning
-
+[02:24.61]夜以继日 不断寻找那片光明
 
 [02:26.90]This ties will never broke again
-
+[02:26.90]我们如胶似漆 谁也无法再次将我们分开
 
 [02:29.10]You will know how much I love you
-
+[02:29.10]你终会明白 我爱你有多深
 
 [02:30.54]Things that I can tell you is wanna be live with you all
-
+[02:30.54]千言万语汇成一句话 我想和你一起共度余生
 
 [02:33.69]And I promise you forever walking with together
-
+[02:33.69]执子之手 与子偕老 这是我对你的海誓山盟
 
 [02:36.12]Giving a thanks to my dears
-
+[02:36.12]我想对你说 谢谢你 我的爱人
 
 [02:38.28]進むべき道ならば
-
+[02:38.28]我们理应前进的道路
 
 [02:42.56]昔から変わらずにある
-
+[02:42.56]从以前开始就未曾改变
 
 [02:46.11]It's just a foolish question
-
+[02:46.11]这个问题太愚蠢了
 
 [02:47.70]もう一人きりじゃ
-
+[02:47.70]独自一人
 
 [02:49.13]越えられない壁も砕かずに
-
+[02:49.13]难以逾越的高墙 无需再奋力
 
 [02:52.08]乗り越えてみよう
-
+[02:52.08]让我们齐心协力共渡难关
 
 [02:53.43]親愛なる
-
+[02:53.43]和亲爱的同伴一起
 
 [02:54.59]仲間たちと光満ちる
-
+[02:54.59]在这洋溢着万丈光芒的
 
 [02:57.20]この世界で
-
+[02:57.20]世界之上
 
 [02:59.42]Can you hear my calling even if its raining
-
+[02:59.42]你能听见我的呼唤吗 穿过那沥沥雨声
 
 [03:01.46]Flowers coming into bloom
-
+[03:01.46]繁花盛开 争相斗艳
 
 [03:03.73]Looking for the shining every night and morning
-
+[03:03.73]夜以继日 不断寻找那片光明
 
 [03:06.07]This ties will never broke again
-
+[03:06.07]我们如胶似漆 谁也无法再次将我们分开
 
 [03:08.22]You will know how much I love you
-
+[03:08.22]你终会明白 我爱你有多深
 
 [03:09.98]Things that I can tell you is wanna be live with you all
-
+[03:09.98]千言万语汇成一句话 我想和你一起共度余生
 
 [03:12.94]And I promise you forever walking with together
-
+[03:12.94]执子之手 与子偕老 这是我对你的海誓山盟
 
 [03:15.27]Giving a thanks to my dears
-
+[03:15.27]我想对你说 谢谢你 我的爱人
 
 [03:17.53]Can you hear my calling even if its raining
-
+[03:17.53]你能听见我的呼唤吗 穿过那沥沥雨声
 
 [03:19.81]Flowers coming into bloom
-
+[03:19.81]繁花盛开 争相斗艳
 
 [03:22.20]Looking for the shining every night and morning
-
+[03:22.20]夜以继日 不断寻找那片光明
 
 [03:24.53]This ties will never broke again
-
+[03:24.53]我们如胶似漆 谁也无法再次将我们分开
 
 [03:26.73]You will know how much I love you
-
+[03:26.73]你终会明白 我爱你有多深
 
 [03:28.17]Things that I can tell you is wanna be live with you all
-
+[03:28.17]千言万语汇成一句话 我想和你一起共度余生
 
 [03:31.32]And I promise you forever walking with together
-
+[03:31.32]执子之手 与子偕老 这是我对你的海誓山盟
 
 [03:33.73]Giving a thanks to my dears
-
+[03:33.73]我想对你说 谢谢你 我的爱人
 
 [03:36.09]Can you hear my calling even if its raining
-
+[03:36.09]你能听见我的呼唤吗 穿过那沥沥雨声
 
 [03:38.40]Flowers coming into bloom
-
+[03:38.40]繁花盛开 争相斗艳
 
 [03:40.72]Looking for the shining every night and morning
-
+[03:40.72]夜以继日 不断寻找那片光明
 
 [03:43.00]This ties will never broke again
-
+[03:43.00]我们如胶似漆 谁也无法再次将我们分开
 
 [03:45.22]You will know how much I love you
-
+[03:45.22]你终会明白 我爱你有多深
 
 [03:46.65]Things that I can tell you is wanna be live with you all
-
+[03:46.65]千言万语汇成一句话 我想和你一起共度余生
 
 [03:49.77]And I promise you forever walking with together
-
+[03:49.77]执子之手 与子偕老 这是我对你的海誓山盟
 
 [03:52.18]Giving a thanks to my dears
-
+[03:52.18]我想对你说 谢谢你 我的爱人
 
 [03:54.48]Can you hear my calling even if its raining
-
+[03:54.48]你能听见我的呼唤吗 穿过那沥沥雨声
 
 [03:56.82]Flowers coming into bloom
-
+[03:56.82]繁花盛开 争相斗艳
 
 [03:59.05]Looking for the shining every night and morning
-
+[03:59.05]夜以继日 不断寻找那片光明
 
 [04:01.43]This ties will never broke again
-
+[04:01.43]我们如胶似漆 谁也无法再次将我们分开
 
 [04:03.64]You will know how much I love you
-
+[04:03.64]你终会明白 我爱你有多深
 
 [04:05.10]Things that I can tell you is wanna be live with you all
-
+[04:05.10]千言万语汇成一句话 我想和你一起共度余生
 
 [04:08.16]And I promise you forever walking with together
-
+[04:08.16]执子之手 与子偕老 这是我对你的海誓山盟
 
 [04:10.70]Giving a thanks to my dears
-
+[04:10.70]我想对你说 谢谢你 我的爱人
 
 [04:12.89]Can you hear my calling even if its raining
-
+[04:12.89]你能听见我的呼唤吗 穿过那沥沥雨声
 
 [04:15.28]Flowers coming into bloom
-
+[04:15.28]繁花盛开 争相斗艳
 
 [04:17.82]Looking for the shining every night and morning
+[04:17.82]夜以继日 不断寻找那片光明
 
 
 [04:19.95]This ties will never broke again
-
+[04:19.95]我们如胶似漆 谁也无法再次将我们分开
 
 [04:22.15]You will know how much I love you
-
+[04:22.15]你终会明白 我爱你有多深
 
 [04:23.58]Things that I can tell you is wanna be live with you all
-
+[04:23.58]千言万语汇成一句话 我想和你一起共度余生
 
 [04:26.73]And I promise you forever walking with together
-
+[04:26.73]执子之手 与子偕老 这是我对你的海誓山盟
 
 [04:29.14]Giving a thanks to my dears
+[04:29.14]我想对你说 谢谢你 我的爱人
 
 
 [04:31.37]Can you hear my calling even if its raining
-
+[04:31.37]你能听见我的呼唤吗 穿过那沥沥雨声
 
 [04:33.70]Flowers coming into bloom
-
+[04:33.70]繁花盛开 争相斗艳
 
 [04:36.02]Looking for the shining every night and morning
-
+[04:36.02]夜以继日 不断寻找那片光明
 
 [04:38.44]This ties will never broke again
-
+[04:38.44]我们如胶似漆 谁也无法再次将我们分开
 
 [04:40.61]You will know how much I love you
-
+[04:40.61]你终会明白 我爱你有多深
 
 [04:42.06]Things that I can tell you is wanna be live with you all
-
+[04:42.06]千言万语汇成一句话 我想和你一起共度余生
 
 [04:45.14]And I promise you forever walking with together
+[04:45.14]执子之手 与子偕老 这是我对你的海誓山盟
 
+[04:47.67]Giving a thanks to my dears
+[04:47.67]我想对你说 谢谢你 我的爱人`;
 
-[04:47.67]Giving a thanks to my dears`
